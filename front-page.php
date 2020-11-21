8f0627099ea85437024df23408954b85cb9b1196
erik <e.mompean@dyasolutions.com>
2020-11-21 13:21:38 +0100

    Improved UI

--- front-page.php
+++ front-page.php
@@ -1,49 +1,42 @@
 <?php get_header() ?>
 <div class="front-page">
-    <div class="parallax">
-        <div>
-            <h1 class="secondary-color">HAPPINESS BEGINS</h1>
-            <p><b>Wedding Planners and Event designers</b></p>
-            <p>En Happiness Begins ofrecemos un servicio exclusivo, a medida y demanda de nuestros clientes</p>
-            <p>Es por eso que no tenemos precios cerrados ni acordados; eloboramos un presupuesto personalizado para cada uno de nuestros clientes, centrándonos en sus peticiones y necesidades.</p>
-        </div>
-
+  <div class="parallax">
+    <div>
+      <h1 class="secondary-color">HAPPINESS BEGINS</h1>
+      <p><b>Wedding Planners and Event designers</b></p>
+      <p>En Happiness Begins ofrecemos un servicio exclusivo, a medida y demanda de nuestros clientes</p>
+      <p>Es por eso que no tenemos precios cerrados ni acordados; eloboramos un presupuesto personalizado para cada uno de nuestros clientes, centrándonos en sus peticiones y necesidades.</p>
     </div>
 
-    <div class="container">
-        <h1 class="text-center primary-color">Que necesitas</h1>
-        <div class="types">
+  </div>
 
-            <div class="card">
-                <?php
-                $wedding =  get_template_directory_uri() . '/assets/images/wedding.jpg';
-                ?>
-                <div class="image" style="background-image: url(<?php echo $wedding ?>);"></div>
-                <div class="button-effect">
-                <button class="bttn-pill bttn-md bttn-danger"onclick="window.location.href='<?php echo get_permalink( get_page_by_path( 'wedding' ) ); ?>'">Boda</button>
-
-                </div>
-            </div>
-            <div class="card">
-                <?php
-                $social_event =  get_template_directory_uri() . '/assets/images/evento-social.jpeg';
-                ?>
-                <div class="image" style="background-image: url(<?php echo $social_event ?>);"></div>
-                <button class="bttn-pill bttn-md bttn-danger"onclick="window.location.href='<?php echo get_permalink( get_page_by_path( 'wedding' ) ); ?>'">Evento Social</button>
-
-           
-            </div>
-            <div class="card">
-                <?php
-                $bussines =  get_template_directory_uri() . '/assets/images/entrepreneur.png';
-                ?>
-                <div class="image" style="background-image: url(<?php echo $bussines ?>);"></div>
-                <button class="bttn-pill bttn-md bttn-danger"onclick="window.location.href='<?php echo get_permalink( get_page_by_path( 'wedding' ) ); ?>'">Evento de Empresa</button>
-            </div>
-
+  <div class="container">
+    <h1 class="text-center primary-color">Que necesitas</h1>
+    <div class="page-content">
+      <div class="card wedding-card">
+        <div class="content">
+        
+          <h2 class="title">Boda</h2>
+          <p class="copy">Descubre nestros servicios de diseño, coordinación, integral y dia B.</p>
+          <button class="btn" onclick="window.location.href='<?php echo get_permalink( get_page_by_path( 'wedding' ) ); ?>'" >Ver Más</button>
         </div>
-
+      </div>
+      <div class="card social-event">
+        <div class="content">
+          <h2 class="title">Evento Social</h2>
+          <p class="copy">¡Te montamos y ayudamos a crear un eventazo con nuestro equipo de Event Designers!</p>
+          <button class="btn" onclick="window.location.href='<?php echo get_permalink( get_page_by_path( 'wedding' ) ); ?>'">Ver Más</button>
+        </div>
+      </div> 
+      <div class="card enterprise-event">
+        <div class="content">
+          <h2 class="title">Evento de Empresa</h2>
+          <p class="copy">Haz de tus reuniones y eventos un gran éxito, olvidándote de las preocupaciones.</p>
+          <button class="btn" onclick="window.location.href='<?php echo get_permalink( get_page_by_path( 'wedding' ) ); ?>'">Ver Más</button>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 
 
--- page-wedding.php
+++ page-wedding.php
@@ -7,17 +7,17 @@
     </div>
 
     <div class="types">
-        <div class="card">
+        <div class="simple-card">
             <h1 class="secondary-font text-center bold primary-color">Servicio Integral</h1>
             <p class="main-font text-center">Nos encargamos de la organización integral desde el momento de nuestra contratación. Es un servicio reservado para esas parejas que quieren disfrutar de su boda con el mínimo de preocupaciones.</p>
             <p class="main-font text-center">Incluimos la organización completa de laboda desde la firma del contrato hasta el día B, incluyendo el acompañamiento a los novios en las visitas y reuniones con proveedores, el diseño y la planificacion de su boda.</p>
         </div>
-        <div class="card">
+        <div class="simple-card">
         <h1 class="secondary-font text-center bold primary-color">Asesoramiento</h1>
             <p class="main-font text-center">Con este servicio ayudamos a nuestras parejas resolviendo sus dudas en cuanto a servicios,proveedores, documentación...Ayudamos y aconsejamos en el proceso de búsqueda y selección del proveedor que mejor se adapte a ellos.</p>
             <p class="main-font text-center">Ofrecemos un asesoramiento online y/o presencial en función de las necesidades de nuestros clientes. Además, si nuestros novios lo desean, les hacemos un proceso de seguimiento y/o guía para indicarles los pasos a seguir para que puedan organizarse ellos su boda de la mejor forma posible.</p>
         </div>
-        <div class="card">
+        <div class="simple-card">
             <h1  class="secondary-font text-center bold primary-color">Día de la boda</h1>
         <p class="main-font text-center">El servicio de coordinación del día de la Boda les garantiza a los novios toda la tranquilidad el día más importante del proceso, para que puedan disfrutar sin preocupaciones.</p>
         <p class="main-font text-center">Este servicio incluye la supervisión del montaje, eventos y desmontaje; además de la coordinación y atención a todos los invitados y proveedores para que todo salga según lo contratado.  </p>
